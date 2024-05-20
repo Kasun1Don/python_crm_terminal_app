3a099a4946ff2dd13d8bd6ea46da9475ab3b4d61
Kasun1Don <Kasunsldon@gmail.com>
2024-05-21 02:05:33 +1000

    updated bash

--- src/run_crm.sh
+++ src/run_crm.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
 if ! [[ -x "$(command -v python3)" ]]
+
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r ./requirements.txt
+python3 main.py
+
 then
-  echo 'Error: 
-    This program runs on Python, but it looks like Python is not installed.
-    To install Python, check out https://installpython3.com/' >&2
+  echo "Error: 
+    This program runs on Python3, but it looks like Python3 is not installed.
+    To install Python3, check out https://www.python.org/downloads/" >&2
   exit 1
 fi
