4889c32c2b085adc70011c74c76b545505325828
Kasun1Don <Kasunsldon@gmail.com>
2024-05-20 16:07:47 +1000

    figured out how to check database

--- src/validations.py
+++ src/validations.py
@@ -1,4 +1,19 @@
 import csv
 import re
 
-def selection
+def selection_invalid():
+    print("Invalid input, select from displayed options")
+
+def company_url_validation(company_url):
+    return company_url.startswith("http://") or company_url.startswith("https://")
+
+#check if a given company url exists in the lead database CSV
+def url_exists(company_url, lead_database="leads_appdatabase.csv"):
+    with open (lead_database) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row ["company_url"] == company_url:
+                return True
+    return False
+
+
