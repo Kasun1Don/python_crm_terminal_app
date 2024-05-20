6db7c114b126ed7639779c5abdb95380a95ca506
Kasun1Don <Kasunsldon@gmail.com>
2024-05-20 16:40:06 +1000

    some validations done

--- src/validations.py
+++ src/validations.py
@@ -16,4 +16,33 @@ def url_exists(company_url, lead_database="leads_appdatabase.csv"):
                 return True
     return False
 
+#check if input name contains only alphabetical characters
+def name_validation(name):
+    return name.isalpha() if name else True
 
+def company_name_validation(company_name):
+    return len(company_name) > 0 if company_name else True
+
+#check if the email address is in the correct format
+def email_validation(email):
+    return re.match(r"[^@]+@[^@]+\.[^@]+", email) if email else True
+
+
+def role_validation(role):
+    return role.isalpha() if role else True
+
+#check if status is one of the accepted values
+def status_validation(status):
+    return status in ["qualified", "unqualified", ""] #allows empty status for incomplete rows
+
+
+#prompt for confirmation before adding the lead
+def confirmation_validation(prompt:str, feedback:str):
+    while True:
+        confirmation = input(prompt).upper()
+        if confirmation == "Y":
+            break
+        if confirmation == "N":
+            print(feedback)
+            exit()
+        print("Invalid input, enter Y or N")
